56b5a1c0e925e87e8d5216bb19c37ad9d93226d9
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-08 15:41:00 +0000

    prepping to build the scraper

--- lib/tcm_movie_search/scraper.rb
+++ lib/tcm_movie_search/scraper.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
 class TcmMovieSearch::Scraper
-  
+
+  # scraper_test is used to follow the trail of passing
+  # values through my classes
   def self.scraper_test
     puts "you have accessed the class TcmMovieSearch::Scraper!"
   end
   
+  # final_destination is another test method for following values
   def self.final_destination(input)
     input = input + 4
     input
   end
+  
+  
   
 end
