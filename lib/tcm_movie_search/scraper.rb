f9f8bd5b35e78481e1609a6e086ebc6bca33d2f1
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-14 17:40:18 +0000

    new Scraper class

--- lib/tcm_movie_search/scraper.rb
+++ lib/tcm_movie_search/scraper.rb
@@ -1,31 +1,31 @@
 class TcmMovieSearch::Scraper
+  attr_accessor :parse_page
 
-  @site = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+  # doc = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
 
-  doc = Nokogiri::HTML(open(@site))
+  # @parse_page ||= Nokogiri::HTML(doc) # ||= if @parse_page doesn't equal Nokogiri::HTML(doc), it will.
 
-  movie_title = doc.css("h2 a")
-  description = doc.css("p.description")
-  link = doc.css('h2 a').map { |link| link['href'] }
-  genre = "#{link.first}genre.html"
+  # movie_title = doc.css("h2 a")
+  # description = doc.css("p.description")
+  # link = doc.css('h2 a').map { |link| link['href'] }
+  # genre = "#{link.first}genre.html"
 
-  movie_genre = Nokogiri::HTML(open(genre))
-  genre_even = movie_genre.css("tr.tdrwodd")
-  genre_odd = movie_genre.css("tr.tdreven")
-  title = movie_genre.css("span.db-movietitle")
-  year = movie_genre.css("span.dbyear")
+  # movie_genre = Nokogiri::HTML(open(genre))
+  # genre_even = movie_genre.css("tr.tdrwodd")
+  # genre_odd = movie_genre.css("tr.tdreven")
+  # title = movie_genre.css("span.db-movietitle")
+  # year = movie_genre.css("span.dbyear")
   # will remove the parentheses from the year to aid in search
   # clean_year = year.gsub(/[()]/, "")
 
-  def self.scrape
-    doc = Nokogiri::HTML(open(@site))
-
-    movies = doc.css("h2 a")
-
-    movies.each do |t|
-      title = t.text
-      TcmMovieSearch::Movies.new(title)
-    end
+  def initialize
+    doc = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+    @parse_page ||= Nokogiri::HTML(doc)
+    # ||= if @parse_page doesn't equal Nokogiri::HTML(doc), it will.
   end
+
+  movie_titles = parse_page.css("h2 a")
+
+  movie_description = parse_page.css("p.description")
 
 end
