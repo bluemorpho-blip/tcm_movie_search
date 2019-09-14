e76c613c4240769ec6d7463c9f05d73fc73def41
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-14 18:26:27 +0000

    updated Scraper class

--- lib/tcm_movie_search/scraper.rb
+++ lib/tcm_movie_search/scraper.rb
@@ -1,4 +1,7 @@
-class TcmMovieSearch::Scraper
+require 'nokogiri'
+require 'open-uri'
+require 'httparty'
+class Scraper
   attr_accessor :parse_page
 
   # doc = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
@@ -19,13 +22,26 @@ class TcmMovieSearch::Scraper
   # clean_year = year.gsub(/[()]/, "")
 
   def initialize
-    doc = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+    doc = HTTParty.get("http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule")
     @parse_page ||= Nokogiri::HTML(doc)
     # ||= if @parse_page doesn't equal Nokogiri::HTML(doc), it will.
   end
 
-  movie_titles = parse_page.css("h2 a")
+  def get_movie_titles
+    movie_titles = parse_page.css("h2 a") { |title| title.text }
+  end
 
-  movie_description = parse_page.css("p.description")
+  def get_movie_description
+    movie_description = parse_page.css("p.description") { |description| description.text }
+  end
+
+  scraper = Scraper.new
+  titles = scraper.get_movie_titles
+  descriptions = scraper.get_movie_description
+
+  (0...titles.size).each do |index| # three dots don't include last digit. 0 to titles - 1
+    puts "- - - index: #{index + 1} - - -"
+    puts "title: #{titles[index].text.gsub(/\([^()]*\)/, '')}\nbrief synposis:#{descriptions[index].text}\n"
+  end
 
 end
