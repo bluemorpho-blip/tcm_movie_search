2973fbc54efed259550e40e7d7ebba203e9443c5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-15 15:05:56 +0000

    more Scraper work

--- lib/tcm_movie_search/scraper.rb
+++ lib/tcm_movie_search/scraper.rb
@@ -21,35 +21,33 @@ class Scraper
   # will remove the parentheses from the year to aid in search
   # clean_year = year.gsub(/[()]/, "")
 
-  def initialize
-    # doc = HTTParty.get("http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule")
-    doc = open("http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule")
-    @parse_page ||= Nokogiri::HTML(doc)
-    # ||= if @parse_page doesn't equal Nokogiri::HTML(doc), it will.
+
+  @site = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+  doc = Nokogiri::HTML(open(@site))
+
+  def scraper
+     movie_title = doc.css("h2 a")
+     description = doc.css("p.description")
+     link = doc.css('h2 a').map { |link| link['href'] }
+     genre = "#{link.first}genre.html"
+
+     doc.each.with_index do |data, index|
+       if data.css("p.description").count != 0
+         description = data.css("p.description")
+
+         TcmMovieSearch::Movies.new(description)
   end
 
-  def get_movie_titles
-    movie_titles = @parse_page.css("h2 a") { |title| title.text }
-  end
+  # scraper = Scraper.new
+  # titles = scraper.get_movie_titles
+  # descriptions = scraper.get_movie_description
+  # links = scraper.get_movie_link
 
-  def get_movie_description
-    movie_description = @parse_page.css("p.description") { |description| description.text }
-  end
-
-  def get_movie_link
-    movie_link = @parse_page.css('h2 a').map { |link| link['href'] }
-  end
-
-  scraper = Scraper.new
-  titles = scraper.get_movie_titles
-  descriptions = scraper.get_movie_description
-  links = scraper.get_movie_link
-
-  (0...titles.size).each do |index| # three dots don't include last digit. 0 to titles - 1
-    puts "\n- - - index: #{index + 1} - - -"
-    puts "\ntitle: #{titles[index].text.gsub(/\([^()]*\)/, '')}"
-    puts "\nbrief synposis:#{descriptions[index].text}"
-    puts "\nmovie link: #{links[index]}"
-  end
+  # (0...titles.size).each do |index| # three dots don't include last digit. 0 to titles - 1
+    # puts "\n- - - index: #{index + 1} - - -"
+    # puts "\ntitle: #{titles[index].text.gsub(/\([^()]*\)/, '')}"
+    # puts "\nbrief synposis:#{descriptions[index].text}"
+    # puts "\nmovie link: #{links[index]}"
+  # end
 
 end
