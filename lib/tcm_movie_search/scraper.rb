d1433db5b6020b33a982d1b1ce093062ab0ea563
Ronald Stephens <rstephens@bluemorpho.cc>
2019-09-20 21:52:09 +0000

    working on date

--- lib/tcm_movie_search/scraper.rb
+++ lib/tcm_movie_search/scraper.rb
@@ -1,36 +1,38 @@
-  class TcmMovieSearch::Scraper
-    attr_accessor :date, :time, :title, :description, :cast, :runtime, :link, :year, :genre
+class TcmMovieSearch::Scraper
+  attr_accessor :date, :time, :title, :description, :cast, :runtime, :link, :year, :genre
 
-    @site = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+  @site = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
 
-    def initialize
-      @title = title
-      @description = description
-      @cast = cast
-      @runtime - runtime
-      @link = link
-      @year = year
-      @genre = genre
-      @date = date
-      @time = time
-    end
+  def initialize
+    @title = title
+    @description = description
+    @cast = cast
+    @runtime - runtime
+    @link = link
+    @year = year
+    @genre = genre
+    @date = date
+    @time = time
+  end
 
-    def self.data_scraper(url)
-      Nokogiri::HTML(open(url))
-      rescue OpenURI::HTTPError
-    end
+  def self.data_scraper(url)
+    Nokogiri::HTML(open(url))
+    rescue OpenURI::HTTPError
+  end
 
-    def self.scraper
-      doc = data_scraper(@site)
+  def self.scraper
+    doc = data_scraper(@site)
+    date_doc = data_scraper(@site)
 
-      rows = doc.css("table tr")
+    date_rows = doc.css("#monthschedule tr")
+    rows = doc.css("table tr")
 
       rows.each.with_index do |row, index|
+        @date = date_rows[index - 1].css("h4 graphicDate")
         @description = row.css("p.description").text.strip
         @cast = row.css(".cast").text.strip
-        @runtime = row.css(".lastp").text.gsub(/[^\d]/, '').strip
+        @runtime = row.css("td .lastp").text.gsub(/[^\d]/, '').strip
         @runtime.concat ' mins'
-        @date = rows[index - 1].css("td h4 span.graphicDate").text.strip
         @time = rows[index - 1].css("h1.nws-date").text.strip
         @title = rows[index - 1].css("a").text.gsub(/\([^()]*\)/, '').strip
         @link = rows[index - 1].css("a").map { |link| link['href'] }
@@ -43,22 +45,22 @@
         else
           @link = "no link available"
         end
-      end
     end
-
-    def self.scrape_genre_page
-      begin
-        doc = data_scraper(@genre)
-        @genre = doc.css("tr.tdrwodd").text.gsub(/\n/, '').strip
-        @year = doc.css("span.dbyear").text.gsub(/[()]/, '').strip
-        rescue
-        @genre = "no genre listed"
-      end
-        create_movie_obj
-    end
-
-    def self.create_movie_obj
-      TcmMovieSearch::Movies.new(@date, @time, @title, @year, @description, @cast, @runtime, @link, @genre)
-    end
-
   end
+
+  def self.scrape_genre_page
+    begin
+      doc = data_scraper(@genre)
+      @genre = doc.css("tr.tdrwodd").text.gsub(/\n/, '').strip
+      @year = doc.css("span.dbyear").text.gsub(/[()]/, '').strip
+      rescue
+      @genre = "no genre listed"
+    end
+      create_movie_obj
+  end
+
+  def self.create_movie_obj
+    TcmMovieSearch::Movies.new(@date, @time, @title, @year, @description, @cast, @runtime, @link, @genre)
+  end
+
+end
--- lib/tcm_movie_search/scraper_bu.rb
+++ lib/tcm_movie_search/scraper_bu.rb
@@ -1,7 +1,19 @@
 class TcmMovieSearch::Scraper
-  attr_accessor :title, :description, :cast, :runtime, :link, :year, :genre
+  attr_accessor :date, :time, :title, :description, :cast, :runtime, :link, :year, :genre
 
   @site = "http://www.tcm.com/schedule/monthly.html?ecid=subnavmonthschedule"
+
+  def initialize
+    @title = title
+    @description = description
+    @cast = cast
+    @runtime - runtime
+    @link = link
+    @year = year
+    @genre = genre
+    @date = date
+    @time = time
+  end
 
   def self.data_scraper(url)
     Nokogiri::HTML(open(url))
@@ -10,40 +22,43 @@ class TcmMovieSearch::Scraper
 
   def self.scraper
     doc = data_scraper(@site)
+
     rows = doc.css("table tr")
 
     rows.each.with_index do |row, index|
-      description = row.css("p.description").text.strip
-      cast = row.css(".cast").text.strip
-      runtime = row.css(".lastp").text.gsub(/[^\d]/, '').strip
-      runtime.concat ' mins'
-      title = rows[index - 1].css("a").text.gsub(/\([^()]*\)/, '').strip
-      @ink = rows[index - 1].css("a").map { |link| link['href'] }
-      link = link[0].to_s
+      @description = row.css("p.description").text.strip
+      @cast = row.css(".cast").text.strip
+      @runtime = row.css(".lastp").text.gsub(/[^\d]/, '').strip
+      @runtime.concat ' mins'
+      @date = rows[index - 1].css("td h4 span.graphicDate").text.strip
+      @time = rows[index - 1].css("h1.nws-date").text.strip
+      @title = rows[index - 1].css("a").text.gsub(/\([^()]*\)/, '').strip
+      @link = rows[index - 1].css("a").map { |link| link['href'] }
+      @link = @link[0].to_s
 
-      if link.start_with?("http:")
-        link
-        genre = "#{link}genre.html"
-        scrape_genre(title, description, cast, runtime, link, genre)
+      if @link.start_with?("http:")
+        @link
+        @genre = "#{@link}genre.html"
+        scrape_genre_page
       else
-        link = "no link available"
+        @link = "no link available"
       end
     end
   end
 
-  def self.scrape_genre(title, description, cast, runtime, link, genre)
+  def self.scrape_genre_page
     begin
-      doc = data_scraper(genre)
-      genre = doc.css("tr.tdrwodd").text.gsub(/\n/, '').strip
-      year = doc.css("span.dbyear").text.gsub(/[()]/, '').strip
+      doc = data_scraper(@genre)
+      @genre = doc.css("tr.tdrwodd").text.gsub(/\n/, '').strip
+      @year = doc.css("span.dbyear").text.gsub(/[()]/, '').strip
       rescue
-      genre = "no genre listed"
+      @genre = "no genre listed"
     end
-      create_movie_obj(title, year, description, cast, runtime, link, genre)
+      create_movie_obj
   end
 
-  def self.create_movie_obj(title, year, description, cast, runtime, link, genre)
-    TcmMovieSearch::Movies.new(title, year, description, cast, runtime, link, genre)
+  def self.create_movie_obj
+    TcmMovieSearch::Movies.new(@date, @time, @title, @year, @description, @cast, @runtime, @link, @genre)
   end
 
 end
